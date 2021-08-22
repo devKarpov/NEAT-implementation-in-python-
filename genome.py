5e8838c092021338c4d25ec15bacd103d06559f0
Alexander Karpov <doktoraudi112@gmail.com>
2021-08-22 19:52:17 +0200

    Satte BiasNode = 0 | Lade till func getNodeFromId

--- genome.py
+++ genome.py
@@ -8,7 +8,7 @@ class genome():
         self.inputnodes = 2
         self.outputnodes = 1
         self.layers = 2
-        self.biasnode = -1
+        self.biasnode = None
     
     def getOutputconnections(self, nodeId): #Ger alla enabled connections som en node ska skicka till
         connections = [] #Connections funktionen ska returna
@@ -24,15 +24,20 @@ class genome():
             self.nodes.append(node)
             self.nextnode =+ 1
 
-    def InitBiasNode(self):
-        bias = Node(self.nextnode, 1)
+    def InitBiasNode(self): #Biasnode har värde = 0
+        bias = Node(0, 1)
         self.biasnode = bias
-        self.nextnode =+ 1
     
     def initOutputNodes(self): #Känns som man borde kunna göra det här efter man har init gömda nodsen för att spara på prestanda, Körs egentligne
         for i in range(1, self.outputnodes + 1): #Sätter nextnode som värdet på varje output node och skapar nya nodes med layer = 2 eftersom det är output layer
             node = Node(self.nextnode, 2) #Skapar Node med nextnode som Id och lagret = 1
             self.nodes.append(node)
             self.nextnode =+ 1
+    
+    def getNodeFromId(self, id): #Säger säg självt
+        return self.nodes[id]
+#Fortsätt utifrån att du har connections och nodes(utan node.layer insatt)
+
+
 
     
